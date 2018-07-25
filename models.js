9c38ebf963ead0e67aa986fc108f8e5ff6b4041c
Nadine Malek <nadinemalek@Chriss-MacBook-Air.local>
2018-07-25 09:47:47 -0700

    autorization and initialization

---
+++ models.js
@@ -0,0 +1,70 @@
+var mongoose = require('mongoose');
+
+var taskSchema = mongoose.Schema({
+  subject: {
+    type: String,
+  required: true},
+  day: {
+    type: String,
+  required: true},
+  googleCalEventID: String,
+  reqID: String
+});
+
+var meetingSchema = mongoose.Schema({
+  day: {
+    type: String,
+    required: true
+  },
+  time:{
+    type: String,
+    required: true
+  },
+  invitees: {
+    type: String,
+    required: true
+  },
+  subject: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  mtngLength: {
+    type: String
+  },
+  status: String,
+  createdAt: Date,
+  requesterId: String
+});
+
+var userSchema = mongoose.Schema({
+  google: {
+    accessToken: String,
+    refreshToken: String,
+    profileId: String
+  },
+  slackId: String,
+  slackUsername: String,
+  slackEmail: String,
+  slackDmId:String
+})
+
+var inviteSchema = mongoose.Schema({
+  eventId: String,
+  inviteeId: String,
+  requesterId: String,
+  status:String
+})
+
+var Task = mongoose.model('Task', taskSchema)
+var Meeting = mongoose.model('Meeting', meetingSchema);
+var Invite = mongoose.model('Invite', inviteSchema);
+var User = mongoose.model('User', userSchema);
+
+module.exports = {
+  Task: Task,
+  Meeting: Meeting,
+  Invite: Invite,
+  User: User
+};
--- testing.js
+++ testing.js
@@ -2,7 +2,15 @@ import {RTMClient, WebClient} from '@slack/client'
 import {google} from 'googleapis'
 import express from 'express'
 import bodyParser from 'body-parser'
+var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI;
+var models = require('./models');
 
+
+mongoose.connect(connect);
+mongoose.connection.on('connected', function(){
+  console.log('connected to mongodb!')
+})
 //
 // Google OAuth
 //
@@ -14,15 +22,6 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.REDIRECT_URL
 )
 
-// ask user for access to their calendar
-console.log('open URI:',oauth2Client.generateAuthUrl({
-  access_type: 'offline',
-  state: 'DEMIMAGIC_ID', // meta-data for DB
-  scope: [
-    'https://www.googleapis.com/auth/calendar'
-  ]
-}))
-
 //
 /////////////////////////// Google API create cal event///////////////////////////
 //
@@ -33,12 +32,12 @@ function makeCalendarAPICall(token) {
     process.env.REDIRECT_URL
   )
 
-  oauth2Client.setCredentials(token)
+  oauth2Client.setCredentials(token) //taking on identity of another user, if bob gave me access to his google account, you are becoming bob
 
-  oauth2Client.on('tokens', (tokens) => {
+  oauth2Client.on('tokens', (tokens) => { //if the token is expired, can have new access token
     if (tokens.refresh_token) {
       // store the refresh_token in my database!
-      console.log(tokens.refresh_token);
+      console.log(tokens.refresh_token); //this means there was a new token or expired token need to save back into the database
     }
     console.log(tokens.access_token);
   });
@@ -92,12 +91,12 @@ function makeCalendarAPICall(token) {
   });
 }
 
-makeCalendarAPICall({
-  access_token: '???',
-  token_type: 'Bearer',
-  refresh_token: '???',
-  expiry_date: 1530585071407
-})
+// makeCalendarAPICall({
+//   access_token: '???',
+//   token_type: 'Bearer',
+//   refresh_token: '???', // PAUL IS THE GOATTTTTTTTT
+//   expiry_date: 1530585071407
+// })
 
 //
 /////////////////////// SLACK RTM chat bot///////////////////////////////////////
@@ -110,63 +109,219 @@ const rtm = new RTMClient(token)
 
 rtm.start()
 
+rtm.on('ready', function(event) {
+  web.chat.postMessage({
+    channel: 'DBVGVLJHK',
+    as_user: true,
+    'text': 'Welcome to the Scheduler Bot! Please reply with verify to begin! :)'
+  })
+})
+
 rtm.on('message', function (event) {
-  console.log(event)
-  if(event.previous_message) console.log('@@@@', JSON.stringify(event.previous_message, null,2))
+
+  //console.log(event)
+  if(event.text === 'verify') {
+    const user = event.user;
+    const url = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      state: user, // meta-data for DB
+      scope: [
+        'https://www.googleapis.com/auth/calendar'
+      ]
+    })
+
+    models.User.findOne({slackId: event.user})
+    .then((user) => {
+      if(user===null){
+        console.log('no user')
+        web.chat.postMessage({
+          channel: event.channel,
+          as_user:true,
+          'text': 'Thanks! Please sign up with this link to link your Google Calendar: ' + url
+        })
+
+      } else {
+
+        oauth2Client.setCredentials({access_token: user.google.accessToken, refresh_token:user.google.refreshToken})
+
+        oauth2Client.on('tokens', (tokens) => {
+
+          if (tokens.refresh_token) {
+            // save the two tokens to db
+            User.update({slackId: userId}, {
+              google: {
+                accessToken: tokens.access_token,
+                refreshToken: tokens.refresh_token
+              }
+            }, function(err) {
+                console.log('error updating user', err)
+              })
+
+          //  console.log(tokens.refresh_token);
+          }
+        //  console.log(tokens.access_token);
+        });
+        web.chat.postMessage({
+          channel: event.channel,
+          as_user: true,
+          'text': 'You are already verified! Thanks! :)'
+        })
+      }
+///////////////////////////////////////////////////  danko
+      console.log(event)
+      const conversationId = event.channel;
+      if (event.bot_id){
+        return;
+      }
+      const projectId = process.env.DIALOGFLOW_PROJECT_ID; //https://dialogflow.com/docs/agents#settings
+      // create a new session for a new user that converses with the bot
+      const sessionId = event.user;
+      const query = event.text;
+      const languageCode = 'en-US';
+
+      // Instantiate a DialogFlow client.
+      const dialogflow = require('dialogflow');
+      const sessionClient = new dialogflow.SessionsClient();
+
+      // Define session path
+      const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: event.text,
+            languageCode: languageCode,
+          },
+        },
+      };
+
+      // Send request and log result
+      sessionClient
+        .detectIntent(request)
+        .then(responses => {
+          console.log('Detected intent'); // Detected intent
+          const result = responses[0].queryResult;
+          console.log("result", result)
+          console.log(`  Query: ${result.queryText}`); // Query: hello
+          console.log(`  Response: ${result.fulfillmentText}`);  // Response: Hello! How can I help you?
+          if (result.queryText.search("remind")!== -1) {
+            var length = result.queryText.length;
+            var index = result.queryText.search('remind');
+            var newString = result.queryText.slice(index + 9, length)
+            //var newA = event.text.splice(index)
+            web.chat.postMessage({
+              channel: conversationId,
+              as_user: true,
+              text: 'Task',
+              attachments: [
+                {
+                  "text": "Create a task" + newString,
+                  "fallback": "Shame... buttons aren't supported in this land",
+                  "callback_id": "button_tutorial",
+                  "color": "#3AA3E3",
+                  "attachment_type": "default",
+                  "actions": [
+                    {
+                      "name": "Yes",
+                      "text": "Yes",
+                      "type": "button",
+                      "value": "yes"
+                    },
+                    {
+                      "name": "No",
+                      "text": "No",
+                      "type": "button",
+                      "value": "no"
+                    },
+                    {
+                      "name": "Maybe",
+                      "text": "Maybe",
+                      "type": "button",
+                      "value": "maybe",
+                      "style": "danger"
+                    }
+                  ]
+                }
+              ]
+            })
+            .then((res) => {
+              console.log("Sent", res)
+            })
+            .catch((err) => {
+              console.log("error", err)
+            })
+          }
+          rtm.sendMessage(result.fulfillmentText, conversationId)
+          if (result.intent) {
+            console.log(`  Intent: ${result.intent.displayName}`); // Intent: Default Welcome intent
+          } else {
+            console.log(`  No intent matched.`);
+          }
+        })
+        .catch(err => {
+          console.error('ERROR:', err);
+        });
+    })
+/////////////////////////////////////////////// danko
+//IF TOKENS ARE EXPIRED DO THIS
+
+  }
+})
+
+  //if(event.previous_message) console.log('@@@@', JSON.stringify(event.previous_message, null,2))
   // The RTM client can send simple string messages
   //rtm.sendMessage('Hello there', event.channel, function(err, res) {
   //  console.log(err, res);
   //})
-  if (event.bot_id) return
-
-  web.chat.postMessage({
-    channel: event.channel,
-    as_user: true,
-    'text': 'Would you like to play a game?',
-    //response_url: "", webhook
-    'attachments': [
-      {
-        'text': 'Choose a game to play',
-        'fallback': 'You are unable to choose a game',
-        'callback_id': 'wopr_game',
-        'color': '#3AA3E3',
-        'attachment_type': 'default',
-        'actions': [
-          {
-            'name': 'game',
-            'text': 'Chess',
-            'type': 'button',
-            'value': 'chess'
-          },
-          {
-            'name': 'game',
-            'text': 'Falken\'s Maze',
-            'type': 'button',
-            'value': 'maze'
-          },
-          {
-            'name': 'game',
-            'text': 'Thermonuclear War',
-            'style': 'danger',
-            'type': 'button',
-            'value': 'war',
-            'confirm': {
-              'title': 'Are you sure?',
-              'text': 'Wouldn\'t you prefer a good game of chess?',
-              'ok_text': 'Yes',
-              'dismiss_text': 'No'
-            }
-          }
-        ]
-      }
-    ]
-  })
-    .then((res) => {
-      // `res` contains information about the posted message
-      console.log('Message sent: ', res.ts)
-    })
-    .catch(console.error)
-})
+  // if (event.bot_id) return
+  // web.chat.postMessage({
+  //   channel: event.channel,
+  //   as_user: true,
+  //   'text': 'Would you like to play a game?',
+  //   //response_url: "", webhook
+  //   'attachments': [
+  //     {
+  //       'text': 'Choose a game to play',
+  //       'fallback': 'You are unable to choose a game',
+  //       'callback_id': 'wopr_game',
+  //       'color': '#3AA3E3',
+  //       'attachment_type': 'default',
+  //       'actions': [
+  //         {
+  //           'name': 'game',
+  //           'text': 'Chess',
+  //           'type': 'button',
+  //           'value': 'chess'
+  //         },
+  //         {
+  //           'name': 'game',
+  //           'text': 'Falken\'s Maze',
+  //           'type': 'button',
+  //           'value': 'maze'
+  //         },
+  //         {
+  //           'name': 'game',
+  //           'text': 'Thermonuclear War',
+  //           'style': 'danger',
+  //           'type': 'button',
+  //           'value': 'war',
+  //           'confirm': {
+  //             'title': 'Are you sure?',
+  //             'text': 'Wouldn\'t you prefer a good game of chess?',
+  //             'ok_text': 'Yes',
+  //             'dismiss_text': 'No'
+  //           }
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // })
+//     .then((res) => {
+//       // `res` contains information about the posted message
+//       console.log('Message sent: ', res.ts)
+//     })
+//     .catch(console.error)
+//})
 
 //
 // OAuth & webhook callbacks
@@ -180,10 +335,27 @@ app.use(bodyParser.json())
 app.get(process.env.REDIRECT_URL.replace(/https?:\/\/.+\//, '/'), (req, res) => {
   oauth2Client.getToken(req.query.code, function (err, token) {
     if (err) return console.error(err.message)
+    //console.log('token', token, 'req.query:', req.query) // req.query.state <- meta-data
 
-    console.log('token', token, 'req.query:', req.query) // req.query.state <- meta-data
+      var newUser = new models.User({
+        google: {
+          accessToken: token.access_token,
+          refreshToken: token.refresh_token
+        },
+        slackId: req.query.state
+      })
 
-    res.send('ok')
+      console.log(newUser)
+
+      newUser.save(function(error,result){
+        if(error){
+          console.log(error, 'error saving user')
+        }else {
+          console.log('yay!')
+        }
+      })
+
+    res.send('Congrats! You have successfully connected your Google Calendar :)')
   })
 })
 
@@ -195,33 +367,3 @@ app.post('/slack', (req, res) => {
 
 app.listen(1337)
 //////////////////////////////////DIALOG FLOW /////////////////////////////////////
-// const sessionId = 'demi-chat-1';
-// const dialogflow = require('dialogflow');
-// const sessionClient = new dialogflow.SessionsClient();
-// const sessionPath = sessionClient.sessionPath(process.env.DIALOGFLOW_PROJECT_ID, sessionId);
-//
-// const request = {
-//   session: sessionPath,
-//   queryInput: {
-//     text: {
-//       text: 'Remind me to do chores tomorrow',
-//       languageCode: 'en-US',
-//     },
-//   },
-// };
-//
-// sessionClient.detectIntent(request)
-//   .then(responses => {
-//     const result = responses[0].queryResult;
-//     console.log('Detected intent', result.parameters.fields);
-//     console.log(`  Query: ${result.queryText}`);
-//     console.log(`  Response: ${result.fulfillmentText}`);
-//     if (result.intent) {
-//       console.log(`  Intent: ${result.intent.displayName}`);
-//     } else {
-//       console.log(`  No intent matched.`);
-//     }
-//   })
-//   .catch(err => {
-//     console.error('ERROR:', err);
-//   });
